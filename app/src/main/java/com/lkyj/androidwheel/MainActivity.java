6d1d6e9efaedec2089758618ff804cea53f5906e
wanzz008 <976919592@qq.com>
2019-02-27 14:14:27 +0800

    编写README.md

--- app/src/main/java/com/lkyj/androidwheel/MainActivity.java
+++ app/src/main/java/com/lkyj/androidwheel/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // sss
+        setContentView(R.layout.activity_main); // sss啊啊
     }
 }
